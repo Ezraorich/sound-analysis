edeb7582af367f9f226ed6fd8a112276b8cbabed
Ezraorich <43410356+Ezraorich@users.noreply.github.com>
2021-11-24 20:30:47 +0600

    Update filename_extraction.py

--- filename_extraction.py
+++ filename_extraction.py
@@ -1,4 +1,19 @@
 from pathlib import Path
 import os
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+import librosa
+import csv
+import os
+
+import pandas as pd
+from glob import glob
 filepath = 'D:/folded_name/'
 pathlist = Path(filepath).glob('**/*.wav')
+
+
+DATA_DIR ='D:/folder_name_with_files/'
+filenames = glob(DATA_DIR + '/*.wav')
