a3010364b3f1e956b358ff82c3883ad8e251fc9c
Saltanat Khalyk <43410356+Ezraorich@users.noreply.github.com>
2022-01-03 23:28:59 +0600

    Update filename_extraction.py

--- filename_extraction.py
+++ filename_extraction.py
@@ -17,3 +17,9 @@ pathlist = Path(filepath).glob('**/*.wav')
 
 DATA_DIR ='D:/folder_name_with_files/'
 filenames = glob(DATA_DIR + '/*.wav')
+
+## if filepath is 'D:/SOUNDS/random_file/test/some_X_folder_name\\1591363379163_1.wav'
+def get_sound_id_from_file_path(file_path):
+    a = file_path.split('\\')[-1][:-4]
+ 
+    return a
